eed1890de202b5afbd3ce8d3c3c7ffe142ff7a05
DanielFrancaContaGit <daniel.pe.15y15@gmail.com>
2019-11-13 13:12:20 -0300

    fim

---
+++ core/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Canais
+from .models import Frases
+
+# Register your models here.
+
+admin.site.register(Canais)
+admin.site.register(Frases)
---
+++ core/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Canais(models.Model):
+    primeiro = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.primeiro
+
+class Frases(models.Model):
+    frase = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.frase
